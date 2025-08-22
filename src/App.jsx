64f3eb8ef31105da3447626d37e82213b74ef519
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-08-23 01:46:47 +0530

    imported reactRouter from react-router-dom and setup in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import Home from './pages/Home';
+import Products from './pages/Products';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Home />
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../assets/Dark.svg';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img src={logo} alt="" width="130 px" />
+      <ul>
+        <Link to='/'><li>Home</li></Link>
+        <Link to='/products'> <li>Products</li> </Link>
+        <Link to='/about'><li>About</li> </Link>
+        <Link to='/contact'><li>Contact</li></Link>
+      </ul>
+      <button>Get Started</button>
+    </div>
+  );
+};
+
+export default Navbar;

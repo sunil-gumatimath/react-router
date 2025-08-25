50dfb5a1b04de9c998aa038a38e4078e1158dd69
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-08-25 17:11:49 +0530

    feat(navbar): Link 'Get Started' button to contact page
    Implement navigation for the "Get Started" button using the `useNavigate` hook from `react-router-dom`.
    
    The button now redirects users to the `/contact` page when clicked, providing a functional call-to-action.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import logo from '../assets/Dark.svg';
 import { Link } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
+import { NavLink,useNavigate } from 'react-router-dom';
 const Navbar = () => {
+
+  const navigate = useNavigate();
+
   return (
     <div className="navbar">
       <img src={logo} alt="" width="130 px" />
@@ -12,7 +15,7 @@ const Navbar = () => {
         <NavLink to='/about'><li>About</li> </NavLink>
         <NavLink to='/contact'><li>Contact</li></NavLink>
       </ul>
-      <button>Get Started</button>
+      <button onClick={()=> navigate('/contact', {replace:false})}>Get Started</button>
     </div>
   );
 };

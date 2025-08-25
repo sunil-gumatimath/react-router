0484a629679a11571d123c8e1b4a9277584e2269
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-08-25 16:52:03 +0530

    feat(nav): Highlight active page link
    Refactor the Navbar to use the `NavLink` component instead of `Link`.
    
    This change allows for applying styles to the link of the currently active page. Added a corresponding `.active` class in the CSS to change the link color, providing clear visual feedback to the user about their current location.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import logo from '../assets/Dark.svg';
 import { Link } from 'react-router-dom';
-
+import { NavLink } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className="navbar">
       <img src={logo} alt="" width="130 px" />
       <ul>
-        <Link to='/'><li>Home</li></Link>
-        <Link to='/products'> <li>Products</li> </Link>
-        <Link to='/about'><li>About</li> </Link>
-        <Link to='/contact'><li>Contact</li></Link>
+        <NavLink to='/'><li>Home</li></NavLink>
+        <NavLink to='/products'> <li>Products</li> </NavLink>
+        <NavLink to='/about'><li>About</li> </NavLink>
+        <NavLink to='/contact'><li>Contact</li></NavLink>
       </ul>
       <button>Get Started</button>
     </div>

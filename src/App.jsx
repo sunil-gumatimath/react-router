8db4d0e9859a3f68c0d7a2f0fb7261528802a11e
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-08-25 18:52:33 +0530

    feat: Implement nested routing for contact page
    Refactor the contact page to introduce nested routes for contact information and a contact form.
    - Create `ContactLayout` to serve as the parent layout for these new sub-pages.
    - Add `ContactInfo` and `ContactForm` components to display specific content.
    - Update `App.jsx` to configure the new nested routing structure.
    - Include navigation buttons on the contact page to switch between the info and form views.
    - Add basic styling for the contact navigation buttons.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import ContactForm from './components/ContactForm';
+import ContactInfo from './components/ContactInfo';
+import ContactLayout from './layout/ContactLayout';
 import RootLayout from './layout/RootLayout';
 import About from './pages/About';
-import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Products from './pages/Products';
 import {
@@ -20,11 +21,14 @@ function App() {
         <Route index element={<Home />} />
         <Route path="products" element={<Products />} />
         <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
+        <Route path='contact' element={<ContactLayout/>}>
+          <Route path='info' element={<ContactInfo />}/>
+          <Route path='form' element={<ContactForm />}/>
+        </Route>
       </Route>
     )
   );
-
+  
   return <RouterProvider router={router} />;
 }
 
---
+++ src/components/ContactInfo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ContactInfo = () => {
+  return (
+    <div>
+        <div>Email: example@gmail.com</div>
+        <div>Phone: +1234567890</div>
+        <div>Address: xyz street, Bangalore</div>
+    </div>
+  )
+}
+
+export default ContactInfo
